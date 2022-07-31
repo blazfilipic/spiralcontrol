24f9b5d7d9a81e9c9ac46274c4fd97491430f778
blazfilipic <blaz.filipic.b@gmail.com>
2022-07-31 15:14:21 +0100

    no message

--- SN74HC595_SPI/SN74HC595_SPI.ino
+++ SN74HC595_SPI/SN74HC595_SPI.ino
@@ -1,61 +1,41 @@
-#define latchPin 5
-#define dataPin 16
-#define clockPin 4
+//#define latchPin 5
+#define latchPin 23
+//#define dataPin 16
+#define dataPin 22
+//#define clockPin 4
+#define clockPin 1
 #define outputoff 2
+#define input 4
 
-//             BGR
-/*#define data1 0b1011001110011111
-#define data2 0b10011111
-#define valC 0b001*/
-
+//outputoff --> HIGH - output is disabled
+//outputoff --> LOW - ouput is enabled
 
 void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(outputoff, OUTPUT);
+  pinMode(input, INPUT_PULLUP);
 }
-
-/*uint16_t combine(uint8_t a, uint8_t b)
-{
-    return (a << 8) | b;
-}*/
-
 uint32_t combine(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
 {
     return (a << 24 ) | (b << 16) | (c << 8) | d;
 }
 
-
-/*void go(byte b, byte c) {
-  digitalWrite(latchPin, LOW);
-  digitalWrite(clockPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, data2);
-  shiftOut(dataPin, clockPin, MSBFIRST, data1;
-  digitalWrite(latchPin, HIGH);
-
-  delay(1000);
-}*/
-/*int combine(int a, int b, int c)
-{
-    return (data1 << 16 ) | (data2 << 8) | c;
-}*/
-
-
 void loop() {
-  uint32_t buf = combine(2, 0, 2, 0);
+  uint32_t buf = combine(1, 0,1, 1);
   //delay(10000);
   digitalWrite(outputoff, HIGH);
   digitalWrite(latchPin, LOW);
   digitalWrite(clockPin, LOW);
-  //combine(data1, data2);
+  
   shiftOut(dataPin, clockPin, MSBFIRST, buf >> 24);
   shiftOut(dataPin, clockPin, MSBFIRST, buf >> 16);
   shiftOut(dataPin, clockPin, MSBFIRST, buf >> 8);
   shiftOut(dataPin, clockPin, MSBFIRST, buf);
-  digitalWrite(latchPin, HIGH);
-  digitalWrite(outputoff, LOW);
-
+  digitalWrite(latchPin, HIGH); 
+  digitalWrite(outputoff, LOW); //enable output
+  
   //delay(10000);
   //digitalWrite(outputoff, HIGH);
   //go(valB);
