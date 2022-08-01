86977a4cbbf08bc094c4a451bd090661d604286f
blazfilipic <blaz.filipic.b@gmail.com>
2022-08-01 09:44:14 +0100

    input pins

--- SN74HC595_SPI/SN74HC595_SPI.ino
+++ SN74HC595_SPI/SN74HC595_SPI.ino
@@ -4,8 +4,9 @@
 #define dataPin 22
 //#define clockPin 4
 #define clockPin 1
-#define outputoff 2
-#define input 4
+#define outputoff 3
+#define input 21
+#define execute 19
 
 //outputoff --> HIGH - output is disabled
 //outputoff --> LOW - ouput is enabled
@@ -16,28 +17,48 @@ void setup() {
   pinMode(dataPin, OUTPUT);
   pinMode(outputoff, OUTPUT);
   pinMode(input, INPUT_PULLUP);
+  pinMode(execute, INPUT_PULLUP);
 }
 uint32_t combine(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
 {
     return (a << 24 ) | (b << 16) | (c << 8) | d;
 }
 
-void loop() {
-  uint32_t buf = combine(1, 0,1, 1);
-  //delay(10000);
-  digitalWrite(outputoff, HIGH);
+void turn_spiral(void)
+{
+  uint32_t buf = combine(1, 0, 0, 9);
   digitalWrite(latchPin, LOW);
   digitalWrite(clockPin, LOW);
-  
   shiftOut(dataPin, clockPin, MSBFIRST, buf >> 24);
   shiftOut(dataPin, clockPin, MSBFIRST, buf >> 16);
   shiftOut(dataPin, clockPin, MSBFIRST, buf >> 8);
   shiftOut(dataPin, clockPin, MSBFIRST, buf);
   digitalWrite(latchPin, HIGH); 
-  digitalWrite(outputoff, LOW); //enable output
-  
-  //delay(10000);
-  //digitalWrite(outputoff, HIGH);
-  //go(valB);
-  //go(valC);
+  digitalWrite(outputoff, LOW);
 }
+
+void loop() {
+  if(digitalRead(execute) == LOW)
+  //digitalWrite(outputoff, HIGH);
+  turn_spiral();
+  if(digitalRead(input) == LOW){
+    digitalWrite(outputoff, HIGH);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
